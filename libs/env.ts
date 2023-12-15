f87360f40873ec8da92bfed7ee7874707e11ad5b
epikoder <efedua.bell@gmail.com>
2023-12-15 14:54:23 +0100

    ensure type match to defaultValue

--- libs/env.ts
+++ libs/env.ts
@@ -1,8 +1,15 @@
-export default function env<T = string | boolean | number>(
+export default function env<T extends string | boolean | number>(
   key: string,
   v: T | undefined,
-) {
+): T {
   const i = Deno.env.get(key);
-  if (!i && v) return v;
-  return <T> i;
+  const value = i ?? v;
+  switch (typeof v) {
+    case "boolean":
+      return <T> !!value;
+    case "number":
+      return <T> parseInt(value?.toString() || "");
+    default:
+      return <T> <string> value;
+  }
 }
