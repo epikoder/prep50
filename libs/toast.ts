441fdaa70bbd06c451e3b2d0503f989c8c9c8f06
epikoder <efedua.bell@gmail.com>
2024-05-26 10:41:10 +0100

    Added style for toast

--- libs/toast.ts
+++ libs/toast.ts
@@ -16,10 +16,11 @@ export {
 }
 
 function showToast(text: string, status: ToastStatus, options?: Omit<Toastify.Options, "text" | 'class'>) {
-    const style = status == 'success' ? "text-blue" : ""
+    const style = status == 'success' ? "bg-[#46b5ff]" : status == 'warning' ? 'bg-yellow-500' : 'bg-red-500'
     Toastify({
         ...options,
-        className: `absolute z-[9999999] px-4 bg-blue-500 ${style}`,
+        className: `top-12 right-6 absolute z-[9999999] px-4 rounded-lg px-4 py-2 text-white text-xs ${style}`,
+        position: 'left',
         text
     }).showToast()
 }
